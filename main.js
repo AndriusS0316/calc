a054ce31c076a337cce588b623f8450d47675a54
Andrius <andriuss0316@gmail.com>
2021-08-24 10:16:35 +0300

    20210824

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // calc uzduotis
 
-let skaicius = 3;
+let skaicius = 2;
 
 const input = document.querySelector(".form1")
 const changeDOM = document.querySelector(".change");
@@ -8,7 +8,10 @@ const secondDOM = document.querySelector(".second");
 const ats = document.querySelector(".number");
 const second = document.querySelector(".second");
 
+
+
 function changeClick() {
+    skaicius = skaicius + 1;
     if (skaicius % 2 === 0) {
         console.log('vienas');
         second.innerText = 'paviršiaus plotas';
@@ -16,15 +19,17 @@ function changeClick() {
         console.log('du');
         second.innerText = 'tūris';
     }
-    skaicius = skaicius + 1;
+    
 }
 
 function secondCick() {
     if (skaicius % 2 === 0) {
         console.log('vienas');
+        second.innerText = 'paviršiaus plotas';
     } else if (skaicius % 2 !== 0) {
         console.log('du');
         second.innerText = 'tūris';
+        document.getElementsByClassName(".turis");
     }
 
 }
