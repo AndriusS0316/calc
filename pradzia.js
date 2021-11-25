e2960453942bce1a1747bffa032d12c97c47bdf4
Andrius <andriuss0316@gmail.com>
2021-11-25 19:26:04 +0200

    20211125

---
+++ pradzia.js
@@ -0,0 +1,15 @@
+// calc uzduotis
+
+
+const more = document.querySelector(".form8");
+
+
+
+function moreClick() {
+    alert("More coming soon...");
+    console.log("More coming soon...");
+}
+
+
+
+more.addEventListener('click', moreClick);
